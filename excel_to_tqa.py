75a8dd85aa24cc59089d326b03f7793b6982eb02
afronhofer <fronhofer@phantomlab.com>
2020-09-21 12:54:01 -0400

    started to add metaItems

--- excel_to_tqa.py
+++ excel_to_tqa.py
@@ -23,13 +23,25 @@ def load_excel_file(excel_file, sheet_dict, sched_idx):
 
     for sheet in sheet_dict:
         excel_sheet = wb.sheet_by_name(sheet['sheetName'])
-        for vars in sheet['sheetVariables']:
-            var_id = tqa.get_variable_id_from_string(vars['name'], sched_idx)
+        for var in sheet['sheetVariables']:
+            var_id = tqa.get_variable_id_from_string(var['name'], sched_idx)
 
-            column_int = abs(65-ord(vars['valueCellColumn'].upper()))
-            val = excel_sheet.cell_value(vars['valueCellRow']-1, column_int)
+            column_int = abs(65-ord(var['valueCellColumn'].upper()))  # **Need to figure out how to find this with double letters
+            val = excel_sheet.cell_value(var['valueCellRow']-1, column_int)
 
             variable_list.append({'id': var_id[0], 'value': val})
+
+            if 'metaItems' in var:
+                meta_items = []
+                for item in var['metaItems']:
+                    meta_column_int = abs(65 - ord(item['valueCellColumn'].upper()))  # **Need to figure out how to find this with double letters
+                    meta_val = excel_sheet.cell_value(item['valueCellRow'] - 1, meta_column_int)
+
+                    meta_items.append({item['name']: meta_val})
+
+                variable_list[-1]['metaItems'] = meta_items
+
+
 
     print(variable_list)
 
