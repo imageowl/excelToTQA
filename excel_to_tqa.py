db3cd8e8995a94e49b8a1c1940b254b41cc1e07d
afronhofer <fronhofer@phantomlab.com>
2020-09-21 13:27:19 -0400

    metaItems working

--- excel_to_tqa.py
+++ excel_to_tqa.py
@@ -9,6 +9,10 @@ import json
 import xlrd
 
 
+def json_print(j):
+    print(json.dumps(j, indent=4, sort_keys=True))
+
+
 def load_json_file(config_file):
     with open(config_file) as file:
         config_dict = json.load(file)
@@ -24,26 +28,34 @@ def load_excel_file(excel_file, sheet_dict, sched_idx):
     for sheet in sheet_dict:
         excel_sheet = wb.sheet_by_name(sheet['sheetName'])
         for var in sheet['sheetVariables']:
-            var_id = tqa.get_variable_id_from_string(var['name'], sched_idx)
+            var_id = tqa.get_variable_id_from_string(var['name'], sched_idx)[0]
 
             column_int = abs(65-ord(var['valueCellColumn'].upper()))  # **Need to figure out how to find this with double letters
             val = excel_sheet.cell_value(var['valueCellRow']-1, column_int)
 
-            variable_list.append({'id': var_id[0], 'value': val})
+            variable_list.append({'id': var_id, 'value': val})
+            print('var_id: ', var_id)
 
             if 'metaItems' in var:
                 meta_items = []
+                sched_vars = tqa.get_schedule_variables(sched_idx)
+                for idx, s in enumerate(sched_vars['json']['variables']):
+                    if s['id'] == var_id:
+                        var_meta_items = s['metaItems']
                 for item in var['metaItems']:
+                    for i in var_meta_items:
+                        if i['name'] == item['name']:
+                            meta_item_id = i['id']
+
                     meta_column_int = abs(65 - ord(item['valueCellColumn'].upper()))  # **Need to figure out how to find this with double letters
                     meta_val = excel_sheet.cell_value(item['valueCellRow'] - 1, meta_column_int)
 
-                    meta_items.append({item['name']: meta_val})
+                    meta_items.append({'id': meta_item_id, 'value': meta_val})
 
                 variable_list[-1]['metaItems'] = meta_items
 
 
-
-    print(variable_list)
+    json_print(variable_list)
 
 
 
