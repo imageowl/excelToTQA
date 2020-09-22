3e8ed69e7a4ddbf0599387ee306c36472cdc5d41
afronhofer <fronhofer@phantomlab.com>
2020-09-22 09:08:30 -0400

    machine and schedule in cells

--- excel_to_tqa.py
+++ excel_to_tqa.py
@@ -21,20 +21,17 @@ def load_json_file(config_file):
 
 def load_excel_file(excel_file, config_file):
     config_dict = load_json_file(config_file)
-
-    machine_idx = tqa.get_machine_id_from_str(config_dict['machineName'])
-    schedule_idx = tqa.get_schedule_id_from_str(config_dict['scheduleName'], machine_idx)
-
-    sheet_dict = config_dict['sheets']
-
+    wb = xlrd.open_workbook(excel_file)
+    sheets_dict = config_dict['sheets']
     variable_list = []
 
-    wb = xlrd.open_workbook(excel_file)
+    sched_id = get_schedule_id(config_dict, wb)
 
-    for sheet in sheet_dict:
+    for sheet in sheets_dict:
         excel_sheet = wb.sheet_by_name(sheet['sheetName'])
+
         for var in sheet['sheetVariables']:
-            var_id = tqa.get_variable_id_from_string(var['name'], schedule_idx)[0]
+            var_id = tqa.get_variable_id_from_string(var['name'], sched_id)[0]
 
             column_int = abs(65-ord(var['valueCellColumn'].upper()))  # **Need to figure out how to find this with double letters
             val = excel_sheet.cell_value(var['valueCellRow']-1, column_int)
@@ -43,7 +40,7 @@ def load_excel_file(excel_file, config_file):
 
             if 'metaItems' in var:
                 meta_items = []
-                sched_vars = tqa.get_schedule_variables(schedule_idx)
+                sched_vars = tqa.get_schedule_variables(sched_id)
                 for idx, s in enumerate(sched_vars['json']['variables']):
                     if s['id'] == var_id:
                         var_meta_items = s['metaItems']
@@ -62,4 +59,25 @@ def load_excel_file(excel_file, config_file):
     json_print(variable_list)
 
 
+def get_schedule_id(config_dict, wb):
+    machine_id = None
+    schedule_id = None
+    sheets_dict = config_dict['sheets']
 
+    for sheet in sheets_dict:
+        excel_sheet = wb.sheet_by_name(sheet['sheetName'])
+        if 'machineCellRow' in sheet:
+            machine_column_int = abs(65 - ord(sheet['machineCellColumn'].upper()))  # **Need to figure out how to find this with double letters
+            machine = excel_sheet.cell_value(sheet['machineCellRow'] - 1, machine_column_int)
+            machine_id = tqa.get_machine_id_from_str(machine)
+        elif 'machineName' in config_dict:
+            machine_id = tqa.get_machine_id_from_str(config_dict['machineName'])
+
+        if 'scheduleCellRow' in sheet:
+            sched_column_int = abs(65 - ord(sheet['scheduleCellColumn'].upper()))  # **Need to figure out how to find this with double letters
+            schedule = excel_sheet.cell_value(sheet['scheduleCellRow'] - 1, sched_column_int)
+            schedule_id = tqa.get_schedule_id_from_str(schedule, machine_id)
+        elif 'scheduleName' in config_dict:
+            schedule_id = tqa.get_schedule_id_from_str(config_dict['scheduleName'], machine_id)
+
+    return schedule_id
