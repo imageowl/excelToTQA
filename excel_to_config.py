2d753f4761f7ddb9aefcf45c24a6f77ba83646bc
afronhofer <fronhofer@phantomlab.com>
2020-09-24 11:58:30 -0400

    added variables to config_dict

--- excel_to_config.py
+++ excel_to_config.py
@@ -2,15 +2,13 @@ import json
 
 import xlrd
 
-machine_name = None
-schedule_name = None
-finalize = None
-mode = None
 
 def json_print(j):
     print(json.dumps(j, indent=4, sort_keys=True))
 
 def excel_to_config_file(excel_file):
+    finalize = None
+    mode = None
     config_dict = {"sheets": []}
     wb = xlrd.open_workbook(excel_file)
 
@@ -20,23 +18,25 @@ def excel_to_config_file(excel_file):
         sheet = wb.sheet_by_name(name)
         config_dict["sheets"].append({"sheetName": name})
 
-        finalize_cell = find_value_in_sheet(sheet, 'Finalize')
-        if finalize_cell is not None:
-            row, col = finalize_cell
-            finalize = int(sheet.cell_value(row+1, col))
-            config_dict["finalize"] = finalize
+        if finalize is None:
+            finalize_cell = find_value_in_sheet(sheet, 'Finalize')
+            if finalize_cell is not None:
+                row, col = finalize_cell
+                finalize = int(sheet.cell_value(row+1, col))
+                config_dict["finalize"] = finalize
 
-        mode_cell = find_value_in_sheet(sheet, 'Mode')
-        if mode_cell is not None:
-            row, col = mode_cell
-            mode = sheet.cell_value(row+1, col)
-            config_dict["mode"] = mode
+        if mode is None:
+            mode_cell = find_value_in_sheet(sheet, 'Mode')
+            if mode_cell is not None:
+                row, col = mode_cell
+                mode = sheet.cell_value(row+1, col)
+                config_dict["mode"] = mode.strip()
 
         machine_name_cell = find_value_in_sheet(sheet, 'Machine Name')
         if machine_name_cell is not None:
             row, col = machine_name_cell
             machine_name = sheet.cell_value(row+1, col)
-            config_dict["machineName"] = machine_name
+            config_dict["machineName"] = machine_name.strip()
         else:
             machine_cell = find_value_in_sheet(sheet, 'machine')
             if machine_cell is not None:
@@ -49,7 +49,7 @@ def excel_to_config_file(excel_file):
         if schedule_name_cell is not None:
             row, col = schedule_name_cell
             schedule_name = sheet.cell_value(row+1, col)
-            config_dict["scheduleName"] = schedule_name
+            config_dict["scheduleName"] = schedule_name.strip()
         else:
             schedule_cell = find_value_in_sheet(sheet, 'schedule')
             if schedule_cell is not None:
@@ -73,11 +73,33 @@ def excel_to_config_file(excel_file):
             config_dict["sheets"][-1]["reportComment"] = {"reportCommentCellRow": report_comment_row,
                                                           "reportCommentCellColumn": report_comment_col}
 
+        variables_table_cell = find_value_in_sheet(sheet, 'Variables Table')
+        if variables_table_cell is not None:
+            config_dict["sheets"][-1]["sheetVariables"] = []
+            row, col = variables_table_cell
+            is_empty = False
+            row_idx = row+2
+            while is_empty is False:
+                variable_name = sheet.cell_value(row_idx, col)
+                if len(variable_name) != 0:
+                    variable_row = int(sheet.cell_value(row_idx, col+1))
+                    variable_col = sheet.cell_value(row_idx, col+2)
+                    config_dict["sheets"][-1]["sheetVariables"].append({"name": variable_name.strip(),
+                                                                        "valueCellRow": variable_row,
+                                                                        "valueCellColumn": variable_col})
+                else:
+                    is_empty = True
+
+                row_idx += 1
+
+
+
 
     json_print(config_dict)
 
+
 def find_value_in_sheet(sheet, val):
-    for colNum in range(sheet.ncols):
-        for rowNum in range(sheet.nrows):
-            if sheet.cell_value(rowNum, colNum) == val:
-                return (rowNum, colNum)
+    for col_num in range(sheet.ncols):
+        for row_num in range(sheet.nrows):
+            if sheet.cell_value(row_num, col_num) == val:
+                return row_num, col_num
