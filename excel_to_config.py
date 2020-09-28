64e70038b060a743da76d831acca2a6d5a6cfddf
afronhofer <fronhofer@phantomlab.com>
2020-09-28 14:20:48 -0400

    small updates

--- excel_to_config.py
+++ excel_to_config.py
@@ -96,9 +96,9 @@ def excel_to_config_file(excel_file):
         if variables_table_cell is not None:  # find table with variables in sheet
             config_dict["sheets"][-1]["sheetVariables"] = []
             variables_table_row, variables_table_col = variables_table_cell
-            is_empty = False
+            cell_is_empty = False
             row_idx = variables_table_row+2
-            while is_empty is False:  # add all variables to config_dict
+            while cell_is_empty is False:  # add all variables to config_dict
                 variable_name = sheet.cell_value(row_idx, variables_table_col).strip()
                 if len(variable_name) != 0:
                     variable_row = int(sheet.cell_value(row_idx, variables_table_col+1))
@@ -110,27 +110,28 @@ def excel_to_config_file(excel_file):
                         # add all meta items to config_dict
                         config_dict["sheets"][-1]["sheetVariables"][-1]["metaItems"] = []
                         meta_items_table_row, meta_items_table_col = find_value_in_sheet(sheet, 'Meta Items Table')
-                        for rowNum in range(meta_items_table_row, sheet.nrows):  # only look in meta items table
-                            found_var = sheet.cell_value(rowNum, meta_items_table_col).strip()
+                        for row_num in range(meta_items_table_row, sheet.nrows):  # only look in meta items table
+                            found_var = sheet.cell_value(row_num, meta_items_table_col).strip()
                             if found_var == variable_name:
-                                meta_item_var_name = sheet.cell_value(rowNum, meta_items_table_col + 1).strip()
-                                meta_items_row = int(sheet.cell_value(rowNum, meta_items_table_col + 2))
-                                meta_items_col = sheet.cell_value(rowNum, meta_items_table_col + 3)
-                                config_dict["sheets"][-1]["sheetVariables"][-1]["metaItems"].append({"name": meta_item_var_name,
-                                                        "valueCellRow": meta_items_row, "valueColumn": meta_items_col})
+                                meta_item_var_name = sheet.cell_value(row_num, meta_items_table_col + 1).strip()
+                                meta_items_row = int(sheet.cell_value(row_num, meta_items_table_col + 2))
+                                meta_items_col = sheet.cell_value(row_num, meta_items_table_col + 3)
+                                config_dict["sheets"][-1]["sheetVariables"][-1]["metaItems"].append(
+                                    {"name": meta_item_var_name, "valueCellRow": meta_items_row,
+                                     "valueColumn": meta_items_col})
 
                     if sheet.cell_value(row_idx, variables_table_col+4).lower() == "yes":  # variable has a variable comment
                         # add variable comment to config_dict
                         comments_table_row, comments_table_col = find_value_in_sheet(sheet, 'Comments Table')
-                        for rowNum in range(comments_table_row, sheet.nrows):  # only look in comments table
-                            found_var = sheet.cell_value(rowNum, comments_table_col).strip()
+                        for row_num in range(comments_table_row, sheet.nrows):  # only look in comments table
+                            found_var = sheet.cell_value(row_num, comments_table_col).strip()
                             if found_var == variable_name:
-                                comment_row = int(sheet.cell_value(rowNum, comments_table_col+1))
-                                comment_col = sheet.cell_value(rowNum, comments_table_col+2)
+                                comment_row = int(sheet.cell_value(row_num, comments_table_col+1))
+                                comment_col = sheet.cell_value(row_num, comments_table_col+2)
                         config_dict["sheets"][-1]["sheetVariables"][-1]["comment"] = {"varCommentCellRow": comment_row,
                                                                                       "varCommentCellColumn": comment_col}
                 else:
-                    is_empty = True  # no more variables present in this sheet
+                    cell_is_empty = True  # no more variables present in this sheet
 
                 row_idx += 1  # check next row for variable
 
@@ -139,7 +140,7 @@ def excel_to_config_file(excel_file):
 
 
 def find_value_in_sheet(sheet, val):
-    # look through entire sheet for specified value
+    # look through entire sheet for specified value, return the indices of the first one found
     for col_num in range(sheet.ncols):
         for row_num in range(sheet.nrows):
             if sheet.cell_value(row_num, col_num) == val:
@@ -147,6 +148,7 @@ def find_value_in_sheet(sheet, val):
 
 
 def write_to_json_file(config_dict):
+    # takes the data in the config_dict and writes it to a local file in json format
     json_object = json.dumps(config_dict, indent=4)
 
     with open("config_file.json", "w") as outfile:
